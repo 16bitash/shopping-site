ad7b2a5c27c4ebd11388119edefeebfe96f62bc2
Ashish Kuamr <entropy.ash@gmail.com>
2017-12-17 15:38:02 +0530

    Configure database URI

--- db/modles.js
+++ db/modles.js
@@ -2,11 +2,7 @@ const Sequelize = require("sequelize");
 const DB = require("../config.json").DB;
 const datatypes = Sequelize.DataTypes;
 
-const db = new Sequelize(DB.DATABASE, DB.USER, DB.PASSWORD, {
-    host: DB.HOST,
-    dialect: DB.DIALECT,
-    // logging: false
-});
+const db = new Sequelize('postgres://vutpvortunrwqj:3eda2f53dccd7642ade2203acf7d3dc9d25cda9bae245cdbeae68013cb28ae2a@ec2-184-72-228-128.compute-1.amazonaws.com:5432/dq1bomrj39o6t');
 
 const Items = db.define('items', {
     itemId: {
